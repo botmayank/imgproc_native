a379ef49d1878270f5e70389ee493a6a802615c1
botmayank@gmail.com <botmayank@gmail.com>
2020-03-21 19:57:44 +0200

    Add OpenCV capture object to Capture

--- include/Capture.h
+++ include/Capture.h
@@ -3,16 +3,20 @@
 
 #include <string>
 #include <memory>
+#include <opencv2/opencv.hpp>
 
 #include "Frame.h"
 
 class Capture {
     public:
         Capture(std::string& path, unsigned int width, unsigned int height);
+        ~Capture();
         void read(std::shared_ptr<Frame> frame);
         unsigned int width, height, size;
+
     private:
         std::string& _path;
+        cv::VideoCapture *cap;
 };
 
 #endif // CAPTURE_H_
--- src/Capture.cpp
+++ src/Capture.cpp
@@ -2,8 +2,17 @@
 
 Capture::Capture(std::string& path, unsigned int width, unsigned int height):
                 width(width), height(height), _path(path) {
-    std::cout << "Initializing Capture" << std::endl;
+    std::cout << "Initializing Capture: " << width << "x" << height << std::endl;
     size = width * height;
+
+    cap = new cv::VideoCapture(_path);
+    if (!cap->isOpened()) {
+        std::cerr << "Failed to open path: " << _path << std::endl;
+    }
+}
+
+Capture::~Capture() {
+    delete cap;
 }
 
 void Capture::read(std::shared_ptr<Frame> frame) {
